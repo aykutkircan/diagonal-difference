7a7ae0a852d801209cebefc0dcc77afc98b1360c
Aykut Kırcan <aykutkircan@icloud.com>
2021-10-22 11:12:20 +0300

    init: first commit diagonal difference

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const {diagonalDifference} = require("./lib/diagonal-difference");
+
+// const arr1 = [
+//   [-1,   1, -7, -8],
+//   [-10, -8, -5, -2],
+//   [ 0,   9,  7, -1],
+//   [ 4,   4, -2,  1]
+// ];
+
+// const a = diagonalDifference(arr1);
+
+module.exports = {
+  diagonalDifference
+}
